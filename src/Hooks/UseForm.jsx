79f25d3a795ef431e5867bdc8c425cef5cc053c8
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2023-11-23 17:02:23 -0300

    Finalizado pagina para postar img

--- src/Hooks/UseForm.jsx
+++ src/Hooks/UseForm.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const validacoes = {
   email: {
     regex:
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    message: 'Preencha um e-mail valido',
+    message: "Preencha um e-mail valido",
+  },
+  number: {
+    regex: /^\d+$/,
+    message: "Utilize numeros apenas",
   },
 };
 
 const UseForm = (type) => {
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState("");
   const [error, setError] = React.useState(null);
 
   function validate(value) {
     if (type === false) return true;
     if (value.length === 0) {
-      setError('Preencha um valor!');
+      setError("Preencha um valor!");
       return false;
     } else if (validacoes[type] && !validacoes[type].regex.test(value)) {
       setError(validacoes[type].message);
