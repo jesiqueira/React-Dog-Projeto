5288f2c093f6a83ddf79da60b7bbd76a1f5ed07d
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2023-11-26 10:26:49 -0300

    update FeedPhotos

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -13,7 +13,7 @@ const FeedPhotos = ({ setModalPhoto }) => {
     async function fetchPhotos() {
       const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
       const { json } = await request(url, options);
-      console.log(json);
+      console.log("JSON: ",json);
     }
     fetchPhotos();
   }, [request]);
